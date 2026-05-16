f93716abcc61d278dc080e7072adff24285c2cae
YunFeng Xu <z5643793@ad.unsw.edu.au>
2026-05-16 22:04:38 +1000

    docs: init the project

---
+++ yun_backend/yun_backend/src/main/java/com/yun/YunBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YunBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YunBackendApplication.class, args);
+    }
+
+}
